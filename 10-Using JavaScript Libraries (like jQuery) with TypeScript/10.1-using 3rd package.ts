6469b769d3f76c50e9bc112735edf6fdbbcbceaf
dongsj <232171773@qq.com>
2016-12-10 18:24:18 +0800

    10... finished

---
+++ 10-Using JavaScript Libraries (like jQuery) with TypeScript/10.1-using 3rd package.ts
@@ -0,0 +1,9 @@
+// npm install --save jquery
+//add jquery.d.ts from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/jquery/jquery.d.ts
+// or add typings
+// npm install -g typeings
+//  typings install dt~jquery --global --save
+// or add npm typings
+// npm install --save-dev @types/jquery
+import 'jQuery';
+$('#app').css({'background-color':'green'});
